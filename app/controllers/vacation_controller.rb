76ef1ca117aa2fcbd0d9da660dccc7ceb5728aad
Bryce Hammond <bryce.hammond@imulus.com>
2014-12-02 14:29:18 -0700

    Fix environment lookup for http auth

--- app/controllers/vacation_controller.rb
+++ app/controllers/vacation_controller.rb
@@ -1,7 +1,7 @@
 class VacationController < ApplicationController
 
   http_basic_authenticate_with name: ENV['VACATION_USER'],
-   password: ENV['VACATION_USER'] if Rails.env != "development"
+   password: ENV['VACATION_PASSWORD'] if Rails.env != "development"
 
   def index
     @imulites = Imulite.pto_eligible.order(:first_name, :last_name)
